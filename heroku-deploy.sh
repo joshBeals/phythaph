11e8f0403c781a9be0e6fc0f0d7a35f859158ed8
JoshuaBeals <joshua@camelcase.co>
2022-08-11 12:33:13 +0100

    Removed unwanted deployment command

--- heroku-deploy.sh
+++ heroku-deploy.sh
@@ -3,7 +3,6 @@
 # cp /app/keys/* /app/storage/
 chmod -R 777 /app/storage/
 touch /app/storage/logs/system.log
-chmod -R 755 /app/modules/
 php /app/artisan migrate
 #php /app/artisan migrate --force
 #php /app/artisan package:discover
