cc9cfb8f93bc38c6ee3f0e2723f4a13062afbac5
JoshuaBeals <joshua@camelcase.co>
2022-09-14 08:54:41 +0100

    Added columns to subscription plan schema

---
+++ database/migrations/2022_09_14_075009_add_columns_to_subscription_plans_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsToSubscriptionPlansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('subscription_plans', function (Blueprint $table) {
+            $table->decimal('min_balance', 10, 2);
+            $table->decimal('max_balance', 10, 2)->nullable()->after('min_balance');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('subscription_plans', function (Blueprint $table) {
+            $table->dropColumn('min_balance');
+            $table->dropColumn('max_balance');
+        });
+    }
+}
